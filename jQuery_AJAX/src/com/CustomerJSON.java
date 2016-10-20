506a3f95a6c85719657c5eb2c5b851c99caae445
fullStackDevs <mr_rip3r@yahoo.com>
2016-10-20 18:09:54 +0300

    Converted project to Maven project and added examples for get and getJSON

--- jQuery_AJAX/src/com/CustomerJSON.java
+++ jQuery_AJAX/src/com/CustomerJSON.java
@@ -9,22 +9,33 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONObject;
+
 /**
  * Servlet implementation class MyServlet
  */
-@WebServlet(description = "Test_AJAX_Servlet", urlPatterns = { "/MyServlet" })
-public class MyServlet extends HttpServlet {
+@WebServlet(description = "Test_AJAX_Servlet", urlPatterns = { "/CustomerJSON_Path" })
+public class CustomerJSON extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    public MyServlet() {
+    public CustomerJSON() {
         // TODO Auto-generated constructor stub
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter writer = response.getWriter();
-		String userName = request.getParameter("userName");
-		writer.println("Hello " + userName);
+		String str_id = request.getParameter("id");
+		int id = 0;
+		if(str_id != null){
+			id = Integer.parseInt(str_id);
+		}
 		
+		JSONObject customer = new JSONObject();
+		customer.put("id", id);
+		customer.put("FirstName", "John");
+		customer.put("LastName", "Doe");
+		
+		writer.print(customer);		
 	}
 
 
